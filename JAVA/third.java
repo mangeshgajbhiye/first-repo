8c4d38983579cd4ae8dca7098ad4b12925979c4d
mangesh <mangeshmaddy0@gmail.com>
2019-07-20 20:21:09 +0530

    third =1000

--- JAVA/third.java
+++ JAVA/third.java
@@ -2,8 +2,8 @@ class Test{
 
  public static void main(String[] args){
 
-         int i=500;
-
+         int i=1000;
+               
 
 	 System.out.println("Hello World");
          System.out.println("this is added portion infile");
