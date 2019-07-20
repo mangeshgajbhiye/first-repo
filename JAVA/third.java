2bcbb8a2686183d0b9e245356d29f4065ff28e60
mangesh <mangeshmaddy0@gmail.com>
2019-07-20 21:14:38 +0530

    i=600

--- JAVA/third.java
+++ JAVA/third.java
@@ -2,7 +2,7 @@ class Test{
 
  public static void main(String[] args){
 
-         int i=700;
+         int i=600;
 
 	 System.out.println("Hello World");
          System.out.println("this is added portion infile");
